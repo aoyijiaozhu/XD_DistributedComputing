9a11f432adf1359c257fddf6f5ed2352478bd344
aoyijiaozhu <122087627+aoyijiaozhu@users.noreply.github.com>
2024-06-14 15:43:07 +0800

    Delete 第四次 directory

--- 第四次/main1.py
+++
@@ -1,9 +0,0 @@
-import client
-import threading
-
-if __name__ == '__main__':
-    nickname=input('输入用户名：')
-    client1 = client.Client(nickname)
-
-    client1_thread = threading.Thread(target=client1.run)
-    client1_thread.start()
--- 第四次/main2.py
+++
@@ -1,9 +0,0 @@
-import client
-import threading
-
-if __name__ == '__main__':
-    nickname = input('输入用户名：')
-    client2 = client.Client(nickname)
-
-    client2_thread = threading.Thread(target=client2.run)
-    client2_thread.start()
